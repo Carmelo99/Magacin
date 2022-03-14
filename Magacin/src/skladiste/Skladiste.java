b0b744fc2c812e4a55a749b04091ecadc56257cf
Carmelo99 <antonije990099@gmail.com>
2022-03-14 17:40:28 +0100

    Dodata klasa Skladiste koja implementira interfejs MagacinInterfejs.

---
+++ Magacin/src/skladiste/Skladiste.java
@@ -0,0 +1,26 @@
+package skladiste;
+
+import artikli.Artikal;
+import interfejsmagacina.MagacinInterfejs;
+
+public class Skladiste implements MagacinInterfejs {
+
+	@Override
+	public void dodajArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void izbaciArtikal(Artikal artikal) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Artikal pronadjiArtikal(int sifra) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
