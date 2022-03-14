ab713099591cce290bd7f39b7edb071ae94d38f9
Carmelo99 <antonije990099@gmail.com>
2022-03-14 17:22:27 +0100

    Napravljen interfejs MagacinInterfejs i definisane metode dodajArtikal, izbaciArtikal i pronadjiArtikal.

---
+++ Magacin/src/interfejsmagacina/MagacinInterfejs.java
@@ -0,0 +1,12 @@
+package interfejsmagacina;
+
+import artikli.Artikal;
+
+public interface MagacinInterfejs {
+
+	public void dodajArtikal(Artikal artikal);
+	public void izbaciArtikal(Artikal artikal);
+	public Artikal pronadjiArtikal(int sifra);
+	
+	
+}
